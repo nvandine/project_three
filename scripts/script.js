447750ca3781fc0e4d28124f204d4d1cdf5f423c
Natalie Van Dine <nvandine@gmail.com>
2018-02-06 17:53:58 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// User sees intro "page" explaining what this adventure will entail
+
+// first page appears with three objects to choose from
+
+// user clicks on the object of choice
+
+// fixed aside's Population Survival Criteria populates
+
+//repeat five more times
+
+// Conclusion page (dream: fixed aside page floats over to be featured); the number of boxes checked determines the number of days/months/years your community survives, with explanation of why
+
+
+
+
+
