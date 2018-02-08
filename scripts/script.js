ce62d7afee49fe2b7503207b38f3e661e35befa7
Natalie Van Dine <nvandine@gmail.com>
2018-02-08 09:12:11 -0500

    javascript functions added, initial html

--- scripts/script.js
+++ scripts/script.js
@@ -16,95 +16,85 @@
 //creating an empty object where the game's functionality lives
 
 const objectFriend = {};  
-   
-//create friend inventory that is an object (array?) that holds the friend data
-
-objectFriend.catalog = [
-        {
-            name: 'spoon',
-            traits: ['cook', 'speedy', 'kind']
-        },
-        {
-            name: 'cathode ray tv',
-            traits: ['kind', 'patient0', 'kicky']
-        },
-        {
-            name: 'party turtle',
-            traits: ['partier', 'fixer', 'archivist']
-        },
-        {
-            name: 'stair car',
-            traits: ['kind', 'waster', 'desperate' ]
-        },
-        {
-            name: 'tuba',
-            traits: ['traitor', 'desperate', 'partier']
-        },
-        {
-            name: 'rainbow trout',
-            traits: ['persuader', 'strong', 'selfish']
-        },
-        {
-            name: 'stadium',
-            traits: ['medical', 'speedy', 'entertainer']
-        },
-        {
-            name: 'glass of milk',
-            traits: ['lazy', 'maker', 'meanie' ]
-        },
-        {
-            name: 'ace of clubs',
-            traits: ['maker','waster','persuader']
-        },
-        {
-            name: 'pantsuit',
-            traits: ['advisor','kicky','selfish']
-        },
-        {
-            name: 'foam finger',
-            traits: ['heroic','medical','entertainer']
-        },
-        {
-            name:'floppy disk',
-            traits: ['archivist', 'strong', 'cook']
-        },
-        {
-            name:'salad',
-            traits: ['meanie', 'fixer', 'heroic']
-        },
-
-]
 
 // key value pairings of traits, values in DAYS
-        const traits = {
-            'heroic': 80,
-            'medical': 200,
-            'cook': 140,
-            'entertainer': 50,
-            'persuader': 90,
-            'fixer': 250,
-            'maker': 175,
-            'archivist': 20,
-            'speedy': 45,
-            'kicky': 35,
-            'strong': 110,
-            'kind': 250,
-            'advisor': 65,
-            'desperate': -90,
-            'partier': -20,
-            'waster': -50,
-            'meanie': -75,
-            'traitor': -200,
-            'selfish': -100,
-            'patient0': -250,
-            'lazy': -30
+objectFriend.traits = {
+    heroic: 80,
+    medical: 200,
+    cook: 140,
+    entertainer: 50,
+    persuader: 90,
+    fixer: 250,
+    maker: 175,
+    archivist: 20,
+    speedy: 45,
+    kicky: 35,
+    strong: 110,
+    kind: 225,
+    advisor: 65,
+    desperate: -90,
+    partier: -20,
+    waster: -50,
+    meanie: -75,
+    traitor: -200,
+    selfish: -100,
+    patient0: -250,
+    lazy: -30
+}
+//create friend inventory that is an object (array?) that holds the friend data
+
+objectFriend.catalog = {
+        spoon: {
+            traits: ['cook', 'speedy', 'kind']
+            // description
+            // title
+        },
+        cathodeRayTv: {
+            traits: ['kind', 'patient0', 'kicky']
+        },
+        partyTurtle: {
+            traits: ['partier', 'fixer', 'archivist']
+        },
+        stairCar: {
+            traits: ['kind', 'waster', 'desperate']
+        },
+        tuba: {
+            traits: ['traitor', 'desperate', 'partier']
+        },
+        rainbowTrout: {
+            traits: ['persuader', 'strong', 'selfish']
+        },
+        stadium: {
+            traits: ['medical', 'speedy', 'entertainer']
+        },
+        glassOfMilk: {
+            traits: ['lazy', 'maker', 'meanie' ]
+        },
+        aceOfClubs: {
+            traits: ['maker','waster','persuader']
+        },
+        pantsuit: {
+            traits: ['advisor','kicky','selfish']
+        },
+        foamFinger: {
+            traits: ['heroic','medical','entertainer']
+        },
+        floppyDisk:{
+            traits: ['archivist', 'strong', 'cook']
+        },
+        salad: {
+            traits: ['meanie', 'fixer', 'heroic']
+            // YOU DON'T MAKE FRIENDS WITH SALAD
         }
+       
+
+}
 
     // write a function (1) that pulls three random objects from MAIN array
-    function getRandomFriend(array){
-        const randoIndex = Math.floor(Math.random()* array.length)
-        return array[randoIndex];
-    },
+    // function getRandomFriend(array){
+    //     const randoIndex = Math.floor(Math.random()* array.length)
+    //     return array[randoIndex];
+    // },
    
     // FOR SAKE OF SIMPLICITY:
     // have all the options on one page - no random, offer selection
@@ -114,6 +104,25 @@ objectFriend.catalog = [
     //QUESTION: can the "name" feed the image that appears on the HTML while the "traits" are stored for future use in a function?
 
     //write a function (2) that, on-click of chosen object, stores the three corresponding traits in a new SURVIVAL array (that can take additional values later)
+   
+    objectFriend.userChoice = function(){
+        let $radios = $('input[type=radio]');
+        console.log($radios);
+        $radios.on('click', function(){
+            console.log(this);
+            let target = this.value;
+            console.log(target);
+            objectFriend.getTraits(target);
+        });
+    }
+
+    objectFriend.getTraits = function(target){
+        // console.log('hello')
+        let traits = objectFriend.catalog[target].traits;
+        console.log(traits);
+
+    }
+
     //stretch goal: display a short description of the chosen object's 
     
     //write a function (3) that removes the three objects from the last turn from the MAIN array
@@ -129,11 +138,13 @@ objectFriend.catalog = [
     // write traits.reduce function to accumulate the value of SURVIVAL array
 
    
-   
+  
    
    
     $(function(){
-
+        // console.log('it works!')
+        
+        objectFriend.userChoice();
     });
 
 
